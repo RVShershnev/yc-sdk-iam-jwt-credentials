2e37ce54e0f0aaf8dd3b2f5316dba203d9c6423c
Roman Shershnev <RVShershnev@yandex.ru>
2023-02-13 04:21:34 +0300

    Added Example

---
+++ examples/YandexCloud.Sdk.IamJwtCredentials.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Yandex.Cloud.Resourcemanager.V1;
+using Yandex.Cloud;
+using System.Text.Json;
+
+namespace YandexCloud.IamJwtCredentials.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IamJwtCredentialsConfiguration configuration = JsonSerializer.Deserialize<IamJwtCredentialsConfiguration>(File.ReadAllText("authorized_key.json"));
+            var sdk = new Sdk(new IamJwtCredentialsProvider(configuration));
+
+            var response = sdk.Services.Ydb.DatabaseService.List(new Yandex.Cloud.Ydb.V1.ListDatabasesRequest() { FolderId = "b1g7r20so8vkbaq4fr1f" });
+                       
+            foreach (var c in response.Databases)
+            {
+                Console.Out.WriteLine($"* {c.Name} ({c.Id})");
+            }
+        }
+    }
+}
